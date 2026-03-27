eef76011fb88c2139b7ee81d61273a5a4f56d599
SuelloNB <ninoberts@gmail.com>
2026-03-27 08:44:52 +0800

    feat: Created a full version of my music player

---
+++ src/script/script.js
@@ -0,0 +1,253 @@
+let songs = [
+    {
+        title: "Aphrodite - The Ridleys",
+        src: "../../assets/audio/Aphrodite.mp3",
+        album: "../../assets/images/aphrodite.jpg",
+        lyrics: `You're the moon that glows in the sky\n
+Lighting up the world when it's blue\n
+Stars they dance though late in the night\n
+Don't you know they dance just for you\n
+There you are above darkened clouds\n
+Smiling at the world from afar\n
+With the stars you wander around\n
+May they follow you wherever you are\n
+Here I am just another boy\n
+Singing songs that others have sung\n
+Trying to find the words to employ\n
+To adore the goddess of love\n
+Oh you got me in a daze yeah\n
+No it's not another phase\n
+You gave me one look and now I can't get my mind off of you\n
+And it's all because\n
+I see the galaxies when I look in your eyes and I\n
+Can't speak no I\n
+Can't speak at all\n
+I swear to Zeus you're Aphrodite in disguise\n
+Don't think that you can hide it from me\n
+Oh no I never thought I'd get this close to someone so divine\n
+Oh I can't breathe no I can't breathe at all\n
+Aphrodite could you could you please be mine oh\n
+Could you please be mine oh mine\n
+Here you are I've waited so long\n
+Hoping you would sit down to stay\n
+Cause with these stars I've been dancing along\n
+Like a fool so you'd look my way\n
+You're the moon that glows in the sky\n
+Lighting up the world when it's blue\n
+And here I sing though late in the night\n
+Hope you know I sing just for you\n
+Source: LyricFind\nSongwriters: Benjamin Manaligod\n`
+    },
+    {
+        title: "Hurts - Lany",
+        src: "../../assets/audio/lany-hurts.mp3",
+        album: "../../assets/images/hurts.jpg",
+        lyrics: 'Here I am again, back to my old ways\n\
+Frozen with my thoughts\n\
+Wasting all my younger days\n\
+And I just can\'t decide, should I be alone?\n\
+Hate the way I am, up every night, glued to my phone\n\
+Please tell me why it\'s easy at first\n\
+But then it all breaks\n\
+The more you love, the more it hurts\n\
+And I was just fine if it didn\'t work\n\
+But now that you\'re mine\n\
+The more I love, the more it hurts, oh\n\
+Honestly, it\'s me, I am so messed up\n\
+Making all the ones who care about me wanna run\n\
+The sunset\'s black and white, it didn\'t used to be\n\
+I remember when it was pink skies, just you and me\n\
+Please tell me why it\'s easy at first\n\
+But then it all breaks\n\
+The more you love, the more it hurts\n\
+And I was just fine if it didn\'t work\n\
+But now that you\'re mine\n\
+The more I love, the more it hurts, oh\n\
+Yeah, oh oh\n\
+The more you love, the more it hurts, oh\n\
+I can\'t help myself\n\
+You\'re the one I want\n\
+It might be bad right now\n\
+But losing you would really suck\n\
+Please tell me why it\'s easy at first\n\
+But then it all breaks\n\
+The more you love, the more it hurts\n\
+And I was just fine if it didn\'t work\n\
+But now that you\'re mine\n\
+The more I love, the more it hurts, oh\n\
+Yeah, oh oh\n\
+The more you love, the more it hurts, oh\n\
+Yeah, oh oh\n\
+The more I love, the more it hurts, oh\n\
+Source: LyricFind\n\
+Songwriters: Charles Leslie Priest / Jake Clifford Goss / Paul Jason Klein'
+    },
+    {
+        title: "LOVE. - Kendrick Lamar",
+        src: "assets/Love..mp3",
+        album: "assets/Love..jpg",
+        lyrics: 'Damn\n\
+Love or lust\n\
+Damn\n\
+All of us\n\
+Give me a run for my money\n\
+There is nobody, no one to outrun me\n\
+(Another world premiere)\n\
+So give me a run for my money\n\
+Sippin\' bubbly, feelin\' lovely\n\
+Livin\' lovely\n\
+Just love me\n\
+I wanna be with you, ayy (just love me)\n\
+I wanna be with (just love me, just love)\n\
+I wanna be with you, ayy (love me), I wanna be with\n\
+I wanna be with you (love me, just love me)\n\
+If I didn\'t ride blade on curb, would you still (love me)?\n\
+If I minimized my net worth, would you still (love me)?\n\
+Keep it a hundred, I\'d rather you trust me than to (love me)\n\
+Keep it a whole one hund\', don\'t got you I got nothin\'\n\
+Ayy, I got somethin\' (I got somethin\'), ayy\n\
+Hol\' up, we gon\' function (we gon\' function), ayy\n\
+No assumptions (no assumptions), ayy\n\
+Feelin\' like Tyson wit\' it (feelin\' like Tyson wit\' it)\n\
+Knock it out twice, I\'m wit\' it (knock it out twice, I\'m wit\' it)\n\
+Only for the night, I\'m kiddin\' (only for the night, I\'m kiddin\')\n\
+Only for life, yeah\n\
+Only for life, yeah, only for life, let\'s get it\n\
+Hit that shoulder lean\n\
+I know what comin\' over me\n\
+Backstroke oversea\n\
+I know what you need\n\
+Already on ten, all money come in\n\
+All feelin\ go out, this feelin\' don\'t drought\n\
+This party won\'t end (love me, love me)\n\
+If I didn\'t ride blade on curb, would you still (love me)?\n\
+If I minimized my net worth, would you still (love me)?\n\
+Keep it a hundred, I\'d rather you trust me than to (love me)\n\
+Keep it a whole one hund\', don\'t got you I got nothin\' (oh, oh)\n\
+Give me a run for my money\n\
+There is nobody, no one to outrun me\n\
+So give me a run for my money\n\
+Sippin\' bubbly, feelin\' lovely\n\
+Livin\' lovely\n\
+Just love me\n\
+I wanna be with you, ayy (just love me)\n\
+I wanna be with (just love me, just love)\n\
+I wanna be with you, ayy (love me), I wanna be with\n\
+I wanna be with you (love me, just love me)\n\
+I\'m on the way\n\
+We ain\'t got no time to waste\n\
+Poppin\' your gum on the way (love me)\n\
+Am I in the way?\n\
+I don\'t want pressure you none\n\
+I want your blessin\' today (love me)\n\
+Oh, by the way, open the door by the way\n\
+Told you that I\'m on the way (love me)\n\
+I\'m on the way, I know connection is vague\n\
+Pick up the phone for me, babe (love me)\n\
+Dammit, we jammin\'\n\
+Bad attitude from your nanny\n\
+Curves and your hips from yo\' mammy\n\
+Remember Gardena? I took the studio camera\n\
+I know Top will be mad at me\n\
+I had to do it, I want your body, your music\n\
+I bought the big one to prove it\n\
+Look what you made\n\
+Told you that I\'m on the way\n\
+I\'m like an exit away, yep\n\
+If I didn\'t ride blade on curb, would you still (love me)?\n\
+If I minimized my net worth, would you still (love me)?\n\
+Keep it a hundred, I\'d rather you trust me than to (love me)\n\
+Keep it a whole one hund\', don\'t got you I got nothin\' (oh, oh)\n\
+Give me a run for my money\n\
+There is nobody, no one to outrun me\n\
+So give me a run for my money\n\
+Sippin\' bubbly, feelin\' lovely\n\
+Livin\' lovely\n\
+Just love me\n\
+I wanna be with you, ayy (just love me)\n\
+I wanna be with (just love me, just love)\n\
+I wanna be with you, ayy (love me), I wanna be with\n\
+I wanna be with you (love me, just love me)\n\
+Source: LyricFind\n\
+Songwriters: Anthony Tiffith / Greg Kursten / Kendrick Lamar / Mark Spears / Teddy Walton / Zacari Pacaldo'
+    },
+    {
+        title: "Pink Skies - Lany",
+        src: "assets/pinkskies.mp3",
+        album: "assets/pinkskies.jpg",
+        lyrics: "Song 3 lyrics line 1\nSong 3 lyrics line 2\nSong 3 lyrics line 3"
+    },
+    {
+        title: "Thick and Thin - Lany",
+        src: "assets/thick.mp3",
+        album: "assets/thick.jpg",
+        lyrics: "Song 3 lyrics line 1\nSong 3 lyrics line 2\nSong 3 lyrics line 3"
+    }
+];
+
+let currentSongIndex = 0; // Start from the first song
+let audio = document.getElementById("audio");
+let songTitle = document.getElementById("song-title");
+let albumCover = document.getElementById("album-cover");
+let lyricsDisplay = document.getElementById("lyrics");
+
+let playBtn = document.getElementById("play-btn");
+let nextBtn = document.getElementById("next-btn");
+let prevBtn = document.getElementById("prev-btn");
+
+playBtn.addEventListener("click", playPause);
+nextBtn.addEventListener("click", nextSong);
+prevBtn.addEventListener("click", prevSong);
+
+
+
+// Automatically play the first song when the page loads
+window.onload = function () {
+    currentSongIndex = 0; // Start with the first song
+    updateSong();
+};
+
+function playPause() {
+    if (audio.paused) {
+        audio.play();
+        playBtn.innerText = "Pause";
+    } else {
+        audio.pause();
+        playBtn.innerText = "Play";
+    }
+}
+
+function nextSong() {
+    currentSongIndex = (currentSongIndex + 1) % songs.length;
+    updateSong();
+}
+
+function prevSong() {
+    currentSongIndex = (currentSongIndex - 1 + songs.length) % songs.length;
+    updateSong();
+}
+
+function updateSong() {
+    lyricsDisplay.innerHTML = "";
+    audio.src = songs[currentSongIndex].src;
+    songTitle.innerText = songs[currentSongIndex].title;
+
+    if (songs[currentSongIndex].album) {
+        albumCover.src = songs[currentSongIndex].album;
+        albumCover.alt = "Album cover for " + songs[currentSongIndex].title;
+    } else {
+        albumCover.src = "default-cover.jpg";
+        albumCover.alt = "Default album cover";
+    }
+
+    audio.load();
+    audio.play().then(() => {
+        playBtn.innerText = "Pause";
+    }).catch(error => {
+        console.error("Autoplay failed:", error);
+    });
+
+    setTimeout(() => {
+        lyricsDisplay.innerText = songs[currentSongIndex].lyrics;
+    }, 50);
+}
